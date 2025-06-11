8412e18fab0fa8c7f0b736a12e98ebb7d540961e
HaroonAli48 <haroonaliali.pro@gmail.com>
2025-06-11 15:03:34 +0500

    .

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
             <p className='hidden md:block'>List Items</p>
         </NavLink>
         <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/editProducts">
-            <img className='w-6 h-6' src={assets.image} alt="" />
+            <img className='w-6 h-6' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAiCAYAAAA6RwvCAAAAAXNSR0IArs4c6QAAAYZJREFUWEftl91NgzEMRW83gUmATWCS0klgE8oksAn0SJ+lNE0c5wepSM1L+xAnx9dOvpudrmTsroRDN5C8Ev9SkbtGP33P9FtEkVdJ+8AmR0lPhXmWgAvaAvmQ9CjpIImFvMUASQdxxNtgDZIqDg/ElCDLfJOWQAYBOACogqrvkl5KwR7I1wZQDHRIDCIvlSVW3NMD+dnoySI60nKgBkmYms+S3iTdl0pcA0FKFOkpS6oEANYf/AcKCEapoas3ay9IqRyskTYrEKlCZyqvUKTWE2yUNm0VgomzIB4EilAOfl2IWZBlEDMgoxDWN5yc6R4ZhWDj6hHu7ZEZiGUgsxDLQOxOyC+kntOxpDSlb08PxDJFcpBeiGUglIbNzZOY4WleVsk5XVIa8xQG8Ln5ix6LaCAXp/Uv/IhnGWoN775rjN61eFGjcprnOr6WZ+WjBRDmiFKMDEr5sH2JawY79NIjE1toBISY3K1drNNSJA1ovWtqkKFm7gEZVSMUdwOJ+pGQnCsn/QLih44jt/GtiQAAAABJRU5ErkJggg==' alt="" />
             <p className='hidden md:block'>Edit products</p>
         </NavLink>
 
